3681b2c5e1f0c1d763b3f0f236c498fcc5b44c74
joellebensadon <jbens2004@gmail.com>
2026-02-10 18:40:53 -0500

    Add basic tests for Force add normalize and scale

--- src/test/java/bensadon/physics/ForceTest.java
+++ src/test/java/bensadon/physics/ForceTest.java
@@ -1,4 +1,52 @@
 package bensadon.physics;
-public class ForceTest {
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ForceTest {
+    private final double tolerance = 0.01;
+
+    @Test
+    void add(){
+        //Given
+        Force f1 = new Force(3,4);
+        Force f2 = new Force(1, 2);
+
+        //When
+        Force result = f1.add(f2);
+
+        //Then
+        assertEquals(4.0, result.getX(), tolerance);
+        assertEquals(6.0, result.getY(), tolerance);
+    }
+
+    @Test
+    void normalize(){
+        //Given
+        Force f1 = new Force(3,4);
+
+        //When
+        Force normalized = f1.normalize();
+
+        //Then
+        assertEquals(1.0, normalized.getMagnitude(), tolerance);
+        assertEquals(0.6, normalized.getX(), tolerance);
+        assertEquals(0.8, normalized.getY(), tolerance);
+    }
+
+    @Test
+    void scale(){
+
+        //Given
+        Force f = new Force(3,4);
+
+        //When
+        Force scaled = f.scale(2);
+
+        //Then
+        assertEquals(6.0, scaled.getX(), tolerance);
+        assertEquals(8.0, scaled.getY(), tolerance);
+    }
 
 }
