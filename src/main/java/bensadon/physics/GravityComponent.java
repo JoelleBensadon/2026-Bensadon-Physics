2077f427d6ea71c11029e0861b6043cf0039589b
joellebensadon <jbens2004@gmail.com>
2026-02-18 15:12:25 -0500

    Draw gravity path from t=0 to t=5

---
+++ src/main/java/bensadon/physics/GravityComponent.java
@@ -0,0 +1,42 @@
+package bensadon.physics;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GravityComponent extends JComponent
+{
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+
+        // g.drawLine(0, 0, getWidth(), getHeight());
+        // g.drawOval(30, 45, 20, 20);
+        // g.fillOval(0, 0, 20, 20);
+
+        // gravity force
+        Force gravity = new Force(0, -9.8);
+        Force scaledGravity = gravity.scale(0.001);
+
+        // force value counted in class
+        Force f1 = new Force(37.0365, 28.9360);
+
+        double x = 0;
+        double y = 0;
+
+        for (double t = 0; t <= 5; t += 0.001)
+        {
+            // add gravity to f1
+            f1 = f1.add(scaledGravity);
+
+            // scale f1 by 0.001
+            Force scaled_f1 = f1.scale(0.001);
+
+            // update x and y using the scaled force
+            x = x + scaled_f1.getX();
+            y = y + scaled_f1.getY();
+
+            g.fillOval((int) x, (int) y, 3, 3);
+        }
+    }
+}
